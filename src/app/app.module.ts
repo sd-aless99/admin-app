efdf4ed8f1a899c7df8a2592a514c0c847420f02
Sara D'Alessio <sara.dalessio7@gmail.com>
2024-10-04 14:50:47 +0200

    Implementacion formulario reactivo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 //componentes de la app
+import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -30,7 +31,8 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class RegisterComponent {
+  
+  registroForm!: FormGroup;
+  
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.registroForm = this.fb.group({
+      nombre: ['', Validators.required],
+      correo: ['', Validators.required, Validators.email],
+      password: ['', Validators.required],
+    });
+  }
+
+  crearUsuario() {
+    
+  }
 
 }
